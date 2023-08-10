21f422b42ecba64ce189912cce241257ba414618
Anzelah <jepkoechanzellah@gmail.com>
2023-08-10 10:53:18 -0700

    task 7

--- console.py
+++ console.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """Defines a module."""
 
+
+import shlex
+import sys
 import cmd
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 """Import the cmd module."""
 
 
 class HBNBCommand(cmd.Cmd):
     """ Class for our airbnb command interpreter."""
     prompt = '(hbnb) '
+    classes = {"BaseModel": BaseModel}
 
     def do_EOF(self, line):
         """An end of file marker."""
@@ -23,6 +29,107 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         pass
+
+    def do_create(self, args):
+        """Create a new instance of basemodel, saves it, and print the id."""
+        if len(args) == 0:
+            print("** class name missing **")
+        
+        try:
+            args = shlex.split(args)
+            instance = eval(args[0])
+            instance.save()
+            print(instance.id)
+        except:
+            print("** class doesn't exist **")
+
+    def do_show(self, args):
+        """Print string representation of an instance
+        based on the class name and id."""
+        args = shlex.split(args)
+        storage = FileStorage()
+
+        if len(args) == 0:
+            print("** class name missing **")
+
+        elif args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+
+        elif len(args) == 1:
+            print("** instance id missing **")
+        
+        elif {args[0].args[1]} not in storage.all():
+            print("** no instance found **")
+            
+        else:
+            print(storage.all()[{args[0].args[1]}])
+
+    def do_destroy(self, args):
+        """ Deletes an instance based on the class name and id."""
+        args = shlex.split(args)
+        storage = FileStorage()
+
+        if len(args) == 0:
+            print("** class name missing **")
+
+        elif (args[0]) not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+
+        elif len(args) == 1:
+            print("** class doesn't exist **")
+
+        elif {args[0].args[1]} not in storage.all():
+            print("** no instance found **")
+
+        else:
+            del storage.all()[args[0].args[1]]
+            storage.save()
+
+    def do_all(self, args):
+        """Prints all string representation of all instances
+        based or not on the class name."""
+        objects = []
+        storage = FileStorage()
+
+        try:
+            if len(args) != 0:
+                eval(args)
+        except NameError:
+            print("** class doesn't exist **")
+            return
+        for key, value in storage.all().items():
+            if len(args) != 0:
+                if type(value) is eval(args):
+                    objects.append(value)
+            else:
+                objects.append(value)
+
+        print(ojects)
+
+    def do_update(self, args):
+        """Updates an instance based on the class name and
+        id by adding or updating attribute."""
+        args = shlex.split(args)
+        storage = FileStorage()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif {args[0].args[1]} not in storage.all():
+            print("** no instance found **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            instance = storage.all()[{args[0].args[1]}]
+            a_type = type(getattr(instance, args[2]))
+            args[3] = a_type(args[3])
+            setattr(instance, args[2], args[3])
+        storage.save()
 
 
 if __name__ == '__main__':
--- models/engine/__init__.py
+++ models/engine/__init__.py
@@ -2,7 +2,7 @@
 """
 Init for models module
 """
-from models.engine.file_storage import Filestorage
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
