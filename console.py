b5cfe8a80574791115f7bf22bbb682aa2e9955d1
Anzelah <jepkoechanzellah@gmail.com>
2023-08-10 11:55:10 -0700

    update the console

--- console.py
+++ console.py
@@ -8,7 +8,12 @@ import cmd
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
-"""Import the cmd module."""
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+"""Import the modules."""
 
 
 class HBNBCommand(cmd.Cmd):
