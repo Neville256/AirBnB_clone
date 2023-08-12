61908986c29f8cda29262348b3422e0bced6147d
Neville256 <nsoundy796@gmail.com>
2023-08-12 09:20:52 -0700

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """
 Custom base class for the entire project
@@ -12,7 +11,7 @@ import models
 class BaseModel:
     """Custom base for all the classes in the AirBnb console project
 
-    Attributes:
+    Arttributes:
         id(str): handles unique user identity
         created_at: assigns current datetime
         updated_at: updates current datetime
@@ -20,54 +19,67 @@ class BaseModel:
     Methods:
         __str__: prints the class name, id, and creates dictionary
         representations of the input values
-        save(self): updates instance attributes with current datetime
-        to_dict(self): returns the dictionary values of the instance obj
+        save(self): updates instance arttributes with current datetime
+        to_dict: returns the dictionary values of the instance obj
 
     """
 
-    DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
+    def __init__(self, *args, **kwargs):
+        """Public instance artributes initialization
+        after creation
 
-    def __init__(self, **kwargs):
-        """Public instance attributes initialization after creation"""
+        Args:
+            *args(args): arguments
+            **kwargs(dict): attrubute values
+
+        """
+        DATE_TIME_FORMAT = '%Y-%m-%dT%H:%M:%S.%f'
         if not kwargs:
             self.id = str(uuid4())
             self.created_at = datetime.utcnow()
             self.updated_at = datetime.utcnow()
             models.storage.new(self)
         else:
-            self.__update_from_dict(kwargs)
-
-    def __update_from_dict(self, kwargs):
-        for key, value in kwargs.items():
-            if key in ("updated_at", "created_at"):
-                setattr(self, key, datetime.strptime(value, self.DATE_TIME_FORMAT))
-            elif key == "id":
-                setattr(self, key, str(value))
-            else:
-                setattr(self, key, value)
+            for key, value in kwargs.items():
+                if key in ("updated_at", "created_at"):
+                    self.__dict__[key] = datetime.strptime(
+                        value, DATE_TIME_FORMAT)
+                elif key[0] == "id":
+                    self.__dict__[key] = str(value)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
-    def __repr__(self):
-        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        """
+        Returns string representation of the class
+        """
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def __repr__(self):
         """Return a string representation of the class. """
 
         return ("[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__))
+                                      self.id, self.__dict__))
 
     def save(self):
+        """
+        Updates the public instance attribute:
+        'updated_at' - with the current datetime
+        """
         self.updated_at = datetime.utcnow()
         models.storage.save()
 
     def to_dict(self):
-        mapped_objects = {}
+        """
+        Method returns a dictionary containing all
+        keys/values of __dict__ instance
+        """
+        map_objects = {}
         for key, value in self.__dict__.items():
             if key == "created_at" or key == "updated_at":
-                mapped_objects[key] = value.isoformat()
+                map_objects[key] = value.isoformat()
             else:
-                mapped_objects[key] = value
-        mapped_objects["__class__"] = self.__class__.__name__
-        return mapped_objects
+                map_objects[key] = value
+        map_objects["__class__"] = self.__class__.__name__
+        return map_objects
